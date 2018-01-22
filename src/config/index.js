6708cab14ae06972eabef7988487873531de7f8a
ramirobg94 <ramirobg94@gmail.com>
2018-01-22 14:01:56 +0100

    merge config layers

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,25 @@ let config;
 const environment = process.env.REACT_APP_ENV || 'local';
 const defaultEnvConfig = require('./environment/default.js');
 const envConfig = require(`./environment/${environment}`);
-config = Object.assign(defaultEnvConfig, envConfig);
+
+const MergeRecursive = (obj1, obj2) => {
+	Object.keys(obj2).forEach((p) => {
+		try {
+			if (obj2[p].constructor === Object) {
+				obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+			} else {
+				obj1[p] = obj2[p];
+			}
+		} catch (e) {
+			obj1[p] = obj2[p];
+		}
+	});
+	return obj1;
+};
+
+const layers = [defaultEnvConfig, envConfig ];
+
+config = layers.reduce((a,b) => MergeRecursive(a,b), {});
+console.log(config);
 
 export default config;
