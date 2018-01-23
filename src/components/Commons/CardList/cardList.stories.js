f5d1a9f2a67e32d75ba8d2f6fca7012c78ed4562
ramirobg94 <ramirobg94@gmail.com>
2018-01-23 16:04:05 +0100

    fix decorators

--- src/components/Commons/CardList/cardList.stories.js
+++ src/components/Commons/CardList/cardList.stories.js
@@ -1,10 +1,22 @@
-import { Provider } from 'redux';
 import React from 'react';
+import StoryRouter from 'storybook-router';
 import { storiesOf } from '@storybook/react';
 import CardList from './cardList.component.js';
 import data from './mock-data.json';
 
+import { Provider } from 'react-redux';
+
+import configureStore from '../../../configureStore';
+
+const store = configureStore();
+
 storiesOf('CardList', module)
+	.addDecorator(StoryRouter())
+	.addDecorator((render) => (
+		<Provider store={store}>
+			{render()}
+		</Provider>
+	))
 	.add('CardList default', () => (
 		<div className="flex-container" style={{background: '#f2f2f2', padding: '50px'}}>
 			<CardList data={data} />
