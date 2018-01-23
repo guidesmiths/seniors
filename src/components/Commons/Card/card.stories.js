516a2dbba5ce8348799f118cbd23032ec74c49e8
feliun <felipe.polo@gmail.com>
2018-01-23 13:29:18 +0100

    Card component ready for storybook

--- src/components/Commons/Card/card.stories.js
+++ src/components/Commons/Card/card.stories.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Card from './card.component.js';
+import data from './mock-data.json';
 
 storiesOf('Card', module)
 	.add('Card default', () => (
 		<div className="flex-container" style={{background: '#f2f2f2', padding: '50px'}}>
-			<Card />
+			<Card {...data} />
 		</div>
 	));
