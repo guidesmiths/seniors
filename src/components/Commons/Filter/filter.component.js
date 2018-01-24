7f19b33149fef26cbf141200026476200eae479c
feliun <felipe.polo@gmail.com>
2018-01-24 16:12:10 +0100

    Making filter a stateless component

--- src/components/Commons/Filter/filter.component.js
+++ src/components/Commons/Filter/filter.component.js
@@ -2,82 +2,60 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./filter.css";
 
-import { FormGroup, Label, Input } from 'reactstrap';
+const Filter = ({ setFilter, filter: { duties, priceRange, postCode, languages } }) =>
+	<div className="col-md-3 flex flex-1 flex-column filterVerBar">
+		<div className="filterItem">
+			<strong style={{fontSize: '1.5em'}}>¿Cómo es tu ayudante ideal?</strong>
+			<br></br>
+		</div>
+		<div className="filterItem">
 
-const duties = ['lavar', 'planchar', 'cocinar'];
+			<strong> ¿Qué sabe hacer? </strong>
+			<br></br>
+			{Object.keys(duties).map((duty) =>
+				<MyCheckbox
+					key={duty + new Date().getTime()}
+					skill={duty} checked={duties[duty]}
+					onChange={(checked) => setFilter( { [duty]: checked}, 'duties')}
+				/>
+			)}
 
-export default class Filter extends React.Component {
+		</div>
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			range: {
-				from: 5,
-				to: 10
-			}
-		};
-	}
+		<div className="filterItem">
+			<strong> ¿Qué idiomas sabe hablar? </strong>
+			<br></br>
+			{Object.keys(languages).map((lang) =>
+				<MyCheckbox
+					key={lang + new Date().getTime()}
+					skill={lang} checked={languages[lang]}
+					onChange={(checked) => setFilter( { [lang]: checked}, 'languages')}
+				/>
+			)}
+		</div>
 
-	render() {
-		console.log(this.props);
-		const { duties, priceRange, postCode, languages } = this.props.filter;
+		<div className="filterItem">
+			<strong> Precio </strong>
+			<TwoValuesSlider
+				from={priceRange.from}
+				to={priceRange.to}
+				setFrom={(value) => setFilter({from: value}, 'priceRange')}
+				setTo={(value) => setFilter({to: value}, 'priceRange')}
+			/>
+		</div>
+		<div className="filterItem">
+			<strong> Código Postal </strong>
+			<input
+				className="inputTextFilter"
+				name="postalCode"
+				type="text"
+				value={postCode}
+				onChange={(e)=> setFilter(e.target.value, 'postCode')} />
+		</div>
 
-		return(
-			<div className="col-md-3 flex flex-1 flex-column filterVerBar">
-				<div className="filterItem">
-					<strong style={{fontSize: '1.5em'}}>¿Cómo es tu ayudante ideal?</strong>
-					<br></br>
-				</div>
-				<div className="filterItem">
+	</div>;
 
-					<strong> ¿Qué sabe hacer? </strong>
-					<br></br>
-					{Object.keys(duties).map((duty) =>
-						<MyCheckbox
-							key={duty + new Date().getTime()}
-							skill={duty} checked={duties[duty]}
-							onChange={(checked) => this.props.setFilter( { [duty]: checked}, 'duties')}
-						/>
-					)}
-
-				</div>
-
-				<div className="filterItem">
-					<strong> ¿Qué idiomas sabe hablar? </strong>
-					<br></br>
-					{Object.keys(languages).map((lang) =>
-						<MyCheckbox
-							key={lang + new Date().getTime()}
-							skill={lang} checked={languages[lang]}
-							onChange={(checked) => this.props.setFilter( { [lang]: checked}, 'languages')}
-						/>
-					)}
-				</div>
-
-				<div className="filterItem">
-					<strong> Precio </strong>
-					<TwoValuesSlider
-						from={priceRange.from}
-						to={priceRange.to}
-						setFrom={(value) => this.props.setFilter({from: value}, 'priceRange')}
-						setTo={(value) => this.props.setFilter({to: value}, 'priceRange')}
-					/>
-				</div>
-				<div className="filterItem">
-					<strong> Código Postal </strong>
-					<input
-						className="inputTextFilter"
-						name="postalCode"
-						type="text"
-						value={postCode}
-						onChange={(e)=> this.props.setFilter(e.target.value, 'postCode')} />
-				</div>
-				
-			</div>
-		);
-	}
-
-}
+export default Filter;
 
 const MyCheckbox = ({ skill, checked, onChange }) =>
 	<div>
