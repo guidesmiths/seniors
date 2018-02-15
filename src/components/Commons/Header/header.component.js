e350b1f0d5f1d42a5bdf1ab28da7b0d503ebb87a
Tokytome <killrei@gmail.com>
2018-02-15 19:06:35 +0100

    refactoring header

--- src/components/Commons/Header/header.component.js
+++ src/components/Commons/Header/header.component.js
@@ -3,37 +3,59 @@ import PropTypes from "prop-types";
 import styled from 'styled-components';
 import logo from '../../../img/logo.svg';
 
-const HeaderStyle = styled.div`
-padding: 12px 16px;
-position: fixed;
-display: flex;
-justify-content: space-between;
-align-items: center;
-width: 100%;
-background-color: transparent;
+const HeaderStyle = styled.div `
+	background-color: rgba(#fff, 0);
+	color: #fff;
+	display: flex;
+	font-weight: 700;
+	font-size: 12px;
+	justify-content: space-between;
+	padding-bottom: 12px;
+	padding-left: 16px;
+	padding-right: 16px;
+	padding-top: 12px;
+	position: fixed;
+	text-decoration: none;
+	width: 100%;
+	z-index: 1;
+	nav{
+		align-items: center;
+		display: flex;
+		ul {
+			list-style: none;
+			margin: 0;
+			padding: 0;
+			a {
+				color: #fff;
+			}
+		}
+	}
+	@media (min-width: 768px) {
+		font-size: 16px;
+		padding-bottom: 20px;
+		padding-left: 32px;
+		padding-right: 32px;
+		padding-top: 20px;
+	}
+	@media (min-width: 1024px) {
+		padding-left: 128px;
+		padding-right: 128px;
+	}
 `;
 
-const MenuItem = styled.li `
-text-decoration: none;
-color: white;
-font-weight:bold;
-font-size:12px;
-letter-spacing:-0.1px;
-@media (min-width: 768px) {
-  font-size: 16px;
-}
+const HeaderStyleScroll = HeaderStyle.extend `
+	background-color: rgba(#fff, 100);
+	border-bottom: 1px solid #000;
 `;
 
 export default class Header extends React.Component {
 	render(){
 		return(
 			<HeaderStyle>
-				<img src={logo} alt="logo" height="50vh"/>
+				<img src={logo} alt="logo" height="32px"/>
 				<nav>
-					<ul className='no-decoration'>
-						<MenuItem>
-							<a href="/html/" className= 'hero'>Entrar</a>
-						</MenuItem>
+					<ul>
+						<li><a href="/html/">Entrar</a></li>
 					</ul>
 				</nav>
 			</HeaderStyle>
