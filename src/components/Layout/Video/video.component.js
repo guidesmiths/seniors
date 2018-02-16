0a1d0a4f54e8a6934b341a37af488306db03b0c9
Diana Veiga <killrei@gmail.com>
2018-02-16 19:32:29 +0100

    Add video component (#14)

---
+++ src/components/Layout/Video/video.component.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from 'styled-components';
+
+const VideoWrapper = styled.div `
+	position: relative;
+	padding-bottom: 56.25%;
+	height: 0;
+`;
+
+export default class Video extends React.Component {
+	render(){
+		return(
+			<VideoWrapper>
+				<iframe frameBorder="0" style={{ overflow: 'hidden', height: '100%', width: '100%', position: 'absolute' }} src="https://www.youtube.com/embed/tgbNymZ7vqY"/>
+			</VideoWrapper>
+		);
+	}
+}
