c9fb575d361f2c9ed3cb300152417299b308ace6
GemmaBeferull <gemmabecala@gmail.com>
2018-02-16 17:59:46 +0100

    padding in inputs

--- src/components/Layout/FilterLanding/filterLanding.component.js
+++ src/components/Layout/FilterLanding/filterLanding.component.js
@@ -1,27 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from 'styled-components';
+import  {Flex} from '../../Styles/Flex';
+import  {colors} from '../../Styles/Variables';
 
-const colors = {
-	primary: '#24b8ca',
-	secondary: '#e81e7c',
-	light: 'white'
-};
-
-const InputContainer = styled.div `
-display: flex;
-flex-direction: column;
+const InputContainer = Flex.extend`
+z-index: 1;
 background-color: ${colors.light};
 padding: 36px 64px;
-align-items: center;
 transition: width 1s;
 @media (min-width: 768px) {
-    justify-content:center;
     position:absolute;
     left: 0;
     right: 0;
     width:80vw;
-    flex-direction: row;
     padding: 46px 52px 40px;
     margin-left: auto;
     margin-right:auto;
@@ -34,6 +26,7 @@ const Input = styled.input `
 margin-bottom:12px;
 margin-right:12px;
 border: 1px solid ${colors.primary};
+padding-left:14px;
 height:36px;
 width:192px;
 @media (min-width: 768px) {
@@ -46,6 +39,7 @@ const Select = styled.select `
 background-color: ${colors.light};
 margin-bottom:12px;
 margin-right:12px;
+padding-left:14px;
 border: 1px solid ${colors.primary};
 height:36px;;
 width:192px;
@@ -78,7 +72,7 @@ display: block;
 export default class FilterLanding extends React.Component {
 	render(){
 		return(
-			<InputContainer>
+			<InputContainer >
 				<Select>
 					<option value="Direction">Filtrar por dirección</option>
 					<option value="Price">Filtrar por precio</option>
