edb59473997af94f4028ea48ab6d20f869488175
Tokytome <killrei@gmail.com>
2018-02-26 00:39:07 +0100

    Responsive login

--- src/components/Views/Login/login.component.js
+++ src/components/Views/Login/login.component.js
@@ -5,6 +5,8 @@ import {Button} from '../../Styles/Buttons';
 import {colors} from '../../Styles/Variables';
 import Spinner from '../../Commons/Spinner/spinner.container';
 import Header from '../../Layout/Header/header.component';
+import Google from '../../../img/google.svg';
+import Facebook from '../../../img/facebook.svg';
 
 
 const Background = styled.div `
@@ -12,6 +14,9 @@ const Background = styled.div `
 	width: 100vw;
 	height: 100vh;
 	background-color: ${colors.medium};
+	a {
+		color: ${colors.primary};
+	}
 `;
 
 const Wrap = styled.div `
@@ -23,19 +28,38 @@ const Wrap = styled.div `
 `;
 
 const Modal = styled.div `
+	margin-top: 64px;
 	box-shadow: 0px 3px 7px 0 rgba(0, 0, 0, 0.36);
 	background-color: ${colors.light};
-	width: 33%;
+	width: 80%;
 	margin-left: auto;
 	margin-right: auto;
 	text-align: center;
-	padding-left: 36px;
-	padding-right: 36px;
-	padding-top: 36px;
-	padding-bottom: 36px;
+	padding-left: 18px;
+	padding-right: 18px;
+	padding-top: 18px;
+	padding-bottom: 18px;
+	font-size: 12px;
 	h1 {
 		font-weight: 600;
-		font-size: 36px;
+		font-size: 24px;
+	}
+	.form-links {
+		font-weight: 700;
+	}
+	@media (min-width: 768px) {
+		width: 50%;
+		padding-left: 36px;
+		padding-right: 36px;
+		padding-top: 36px;
+		padding-bottom: 36px;
+		h1 {
+			font-weight: 600;
+			font-size: 36px;
+		}
+	}
+	@media (min-width: 1024px) {
+		width: 33%;
 	}
 `;
 
@@ -50,27 +74,71 @@ const Input = styled.input `
   -webkit-border-radius: 0px;
   &::placeholder {
     color: ${colors.primary};
-	$:focus {
-		color: ${colors.primary};
 	}
 `;
 
 const Social = styled.div `
 	margin-top: 18px;
-	display:flex;
-	justify-content: space-between;
-	button {
-		color: ${colors.light};
-		border-color: red;
-		background-color: red;
+	display: flex;
+	flex-direction: column;
+	@media (min-width: 768px) {
+		flex-direction: row;
+		justify-content: space-between;
 	}
 `;
 
 const AccountOptions = styled.div `
 	margin-top: 18px;
 	color: ${colors.dark};
-	a {
-		color: ${colors.primary};
+`;
+
+const SocialButton = Button.extend  `
+	display:flex;
+	align-items: center;
+	justify-content: space-around;
+	background-color: blue;
+	border-color: blue;
+	padding-left:8px;
+	width: 75%;
+	font-size: 10px;
+	img {
+		border-left: solid 1px white;
+		padding-left: 4px;
+		width:18%;
+	}
+	&:hover {
+		border-color: blue;
+		background-color: white;
+		color: blue;
+		img {
+			border-color: blue;
+		}
+	}
+	&:last-child {
+		margin-top: 14px;
+		background-color: red;
+		border-color: red;
+		img {
+			border-left: solid 1px white;
+			padding-left: 6px;
+			width:20%;
+		}
+		&:hover {
+			background-color: white;
+			color: red;
+			img {
+				border-color: red;
+			}
+		}
+	}
+	@media (min-width: 768px) {
+		width: 49%;
+		font-size: 10px;
+		&:last-child {
+			margin-top: 0;
+		}
+		@media (min-width: 1024px) {
+			width: 45%;
 	}
 `;
 
@@ -142,12 +210,12 @@ export default class Login extends React.Component {
 								<hr/>
 
 								<Social>
-									<button id="submit" type="submit" name="Facebook" social="Facebook">
-											Facebook
-									</button>
-									<button id="submit" type="submit" name="Google" social="Google">
-											Google
-									</button>
+									<SocialButton id="submit" type="submit" name="Facebook">
+										Login with Facebook <img src={Facebook} alt="Facebook-icon"/>
+									</SocialButton>
+									<SocialButton id="submit" type="submit" name="Google">
+										Login with Google<img src={Google} alt="Google-icon"/>
+									</SocialButton>
 								</Social>
 
 								<AccountOptions>
